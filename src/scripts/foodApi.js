88136c60e21fd5d8d68ed7bead92c9d37d0132bc
JD <jdwheeler0@gmail.com>
2018-12-03 14:09:15 -0600

    let's get funky

--- src/scripts/foodApi.js
+++ src/scripts/foodApi.js
@@ -1,5 +1,5 @@
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
-    })
+        console.table(parsedFoods);
+    });
